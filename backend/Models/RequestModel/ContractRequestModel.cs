4c937634266fe843ce4b7e9b86750ff4e4c95f1d
Marcel <marcelpodsiadlo.business@gmail.com>
2025-09-02 18:10:26 +0200

    Created model for PUT endpoint

---
+++ backend/Models/RequestModel/ContractRequestModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class ContractRequestModel
+{
+  public decimal Price { get; set; }
+  public StatusOfContractModel Status { get; set; }
+  public string? Description { get; set; }
+  public DateTime? Deadline { get; set; }
+
+  [ForeignKey("AuthorId")]
+  public UsersModel? Author { get; set; }
+}
