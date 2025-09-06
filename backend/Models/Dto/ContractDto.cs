34e07f033093215f5cfd5c48a42e951d30a5e79d
Marcel <marcelpodsiadlo.business@gmail.com>
2025-09-06 11:44:59 +0200

    Create contract dto for response

---
+++ backend/Models/Dto/ContractDto.cs
@@ -0,0 +1,11 @@
+public class ContractDto
+{
+    public int Id { get; set; }
+    public int AuthorId { get; set; }
+    public decimal Price { get; set; }
+    public StatusOfContractModel Status { get; set; }
+    public string Description { get; set; } = "";
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? Deadline { get; set; }
+}
