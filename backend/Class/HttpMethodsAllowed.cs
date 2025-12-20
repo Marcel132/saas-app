9ddacee5c68be8e01096cde9a47081ce546d2336
Marcel <marcelpodsiadlo.business@gmail.com>
2025-12-20 17:19:17 +0100

    Created list of allowed method

---
+++ backend/Class/HttpMethodsAllowed.cs
@@ -0,0 +1,11 @@
+public static class HttpMethodsAllowed
+{
+  public static readonly string[] Methods = new[]
+  {
+    HttpMethods.Get,
+    HttpMethods.Post,
+    HttpMethods.Put,
+    HttpMethods.Delete,
+    HttpMethods.Patch,
+  };
+}
