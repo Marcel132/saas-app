bb9e81fcd5b55bf4565fba8b1f4b36098643c076
Marcel <marcelpodsiadlo.business@gmail.com>
2025-12-26 23:38:00 +0100

    -Fix: Repair issuess

---
+++ backend/Api/Controllers/ContractsController.cs
@@ -0,0 +1,215 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class ContractsController : ControllerBase
+{
+  private readonly ILogger<ContractsController> _logger;
+  private readonly ContractsService _contractsService;
+  protected Guid? GetUserId()
+  {
+    var user = Guid.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out var userId)
+      ? userId
+      : (Guid?)null;
+    _logger.LogInformation($"{user}");
+    return user;
+  }
+
+  public ContractsController(ILogger<ContractsController> logger, ContractsService contractsService)
+  {
+    _logger = logger;
+    _contractsService = contractsService;
+  }
+
+  // [Authorize]
+  [HttpGet]
+  public async Task<IActionResult> GetContracts()
+  {
+    // Move to [Authorize]
+    var userId = GetUserId();
+    if (userId is null || userId == Guid.Empty)
+      return Unauthorized(new { success = false, message = "You are not allowed to use this method" });
+
+    try
+      {
+        var contracts = await _contractsService.GetAllContractsAsync(userId);
+
+        if (contracts.Count == 0)
+          return NotFound(new { success = false, message = "No contracts found." });
+
+        return Ok(new { success = true, data = contracts, message = "Contracts retrieved successfully." });
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError(ex, "Error retrieving contracts");
+        return StatusCode(500, new { success = false, message = "An error occurred while retrieving contracts." });
+      }
+  }
+
+  // [Authorize]
+  [HttpGet("{id}")]
+  public async Task<IActionResult> GetContractById(Guid id)
+  {
+    if (id == Guid.Empty)
+      return BadRequest(new { success = false, message = "Invalid contract ID." });
+
+    var userId = GetUserId() ?? Guid.Empty;
+    if (userId == Guid.Empty)
+      return Unauthorized(new { success = false, message = "You are not allowed to use this method" });
+
+    try
+    {
+      var contract = await _contractsService.GetContractByIdAsync(id, userId);
+
+      return Ok(new { success = true, data = contract, message = "Contract retrieved successfully." });
+    }
+    catch (ArgumentException ex) { return BadRequest(new { success = false, message = ex.Message }); }
+    catch (KeyNotFoundException ex) { return NotFound(new { success = false, message = ex.Message }); }
+    catch (System.Exception)
+    {
+      return StatusCode(500, new { success = false, message = "An error occurred while retrieving the contract." });
+    }
+  }
+
+  // [Authorize(Roles = "Admin, Company")]
+  [HttpPost]
+  public async Task<IActionResult> CreateContract([FromBody] ContractModel request)
+  {
+    ArgumentNullException.ThrowIfNull(request, "Request cannot be null");
+
+    if (!ModelState.IsValid)
+      return BadRequest(new { success = false, message = "Invalid contract data." });
+
+    var userId = GetUserId() ?? Guid.Empty;
+    if (userId == Guid.Empty)
+      return Unauthorized(new { success = false, message = "You are not allowed to use this method" });
+
+    try
+    {
+
+      var createdContract = await _contractsService.CreateContractAsync(request, userId);
+      ArgumentNullException.ThrowIfNull(createdContract, "Failed to create a contract");
+
+      return Ok(new { success = true, data = createdContract, message = "Contract created successfully." });
+    }
+    catch (ArgumentException ex) { return BadRequest(new { success = false, message = ex.Message }); }
+    catch (Exception ex)
+    {
+      _logger.LogError(ex, "Error creating contract");
+      return StatusCode(500, new { success = false, message = "An error occurred while creating the contract." });
+    }
+  }
+
+  // [Authorize(Roles = "Admin, Company")]
+  // [HttpPut("{id}")]
+  // public async Task<IActionResult> UpdateContract(int id, [FromBody] ContractRequestModel request)
+  // {
+  //   ArgumentNullException.ThrowIfNull(request, "Request must have a value");
+  //   if (id <= 0)
+  //     return BadRequest(new { message = "Invalid contract ID." });
+  //   if (!ModelState.IsValid)
+  //     return BadRequest(new { message = "Invalid contract data." });
+
+  //   var userId = GetUserId() ?? 0;
+  //   if (userId <= 0)
+  //     return Unauthorized(new { success = false, message = "You are not allowed to use this method" });
+
+  //   try
+  //   {
+  //     var isContractUpdated = await _contractsService.UpdateContractAsync(id, request, userId);
+
+  //     if (isContractUpdated == false)
+  //       return NotFound(new { message = $"Contract with ID {id} not found." });
+
+  //     return Ok(new { success = true, message = "Contract updated successfully." });
+  //   }
+  //   catch (ArgumentException ex) { return BadRequest(new { message = ex.Message }); }
+  //   catch (KeyNotFoundException ex) { return NotFound(new { message = ex.Message }); }
+  //   catch (UnauthorizedAccessException ex) { return Unauthorized(new { message = ex.Message }); }
+  //   catch (System.Exception)
+  //   {
+  //     return StatusCode(500, new { success = false, message = "An error occurred while updating a contract" });
+  //   }
+
+  // }
+
+  // [Authorize(Roles = "Admin, Company")]
+  [HttpDelete("{id}")]
+  public async Task<IActionResult> DeleteContract(Guid id)
+  {
+    var userId = GetUserId() ?? Guid.Empty;
+    if (userId == Guid.Empty)
+      return Unauthorized(new { success = false, message = "You are not allowed to use this method" });
+
+    try
+    {
+      await _contractsService.DeleteContractAsync(id, userId);
+
+      return Ok(new { success = true, message = $"Deleted contract with ID: {id}" });
+    }
+    catch (ArgumentException ex) { return BadRequest(new { success = false, message = ex.Message }); }
+    catch (KeyNotFoundException ex) { return NotFound(new { success = false, message = ex.Message }); }
+    catch (UnauthorizedAccessException ex) { return Unauthorized(new { success = false, message = ex.Message }); }
+    catch (System.Exception)
+    {
+      return StatusCode(500, new { success = false, message = "An error occurred while deleteing a contract" });
+    }
+
+  }
+
+  [HttpPost("{id}/apply")]
+  public async Task<IActionResult> ApplyForContract(Guid id)
+  {
+    var userId = GetUserId() ?? Guid.Empty;
+    if (userId == Guid.Empty)
+      return Unauthorized(new { success = false, message = "You are not allowed to use this method" });
+
+    if (id == Guid.Empty)
+      return BadRequest(new { success = false, message = "Invalid contract ID" });
+
+    try
+    {
+      var appliedForContract = await _contractsService.ApplyForContractAsync(id, userId);
+
+      return Ok(new { success = true, data = appliedForContract, message = "Applied successfuly" });
+    }
+    catch (ArgumentException ex) { return BadRequest(new { success = false, message = ex.Message }); }
+    catch (KeyNotFoundException ex) { return BadRequest(new { success = false, message = ex.Message }); }
+    catch (System.Exception)
+    {
+      return StatusCode(500, new { success = false, message = "An error occurred while applying for contract" });
+    }
+  }
+
+  [HttpPut("{contractId}/accept/{userId}")]
+  public async Task<IActionResult> AcceptContract(Guid contractId, Guid userId)
+  {
+    if (contractId == Guid.Empty)
+      return BadRequest(new { success = false, message = "Invalid contract ID" });
+    if (userId == Guid.Empty)
+      return BadRequest(new { success = false, message = "Invalid user ID" });
+
+    var authorId = GetUserId() ?? Guid.Empty;
+    if (authorId == Guid.Empty)
+      return Unauthorized(new { success = false, message = "You are not allowed to use this method" });
+      
+    try
+    {
+      var accepted = await _contractsService.AcceptContractAsync(contractId, userId, authorId);
+      _logger.LogInformation(accepted.ToString());
+
+      return Ok(new { success = true, data = accepted, message = "Contract accepted succesfuly" });
+    }
+    catch (ArgumentException ex) { return BadRequest(new { success = false, message = ex.Message }); }
+    catch (KeyNotFoundException ex) { return BadRequest(new { success = false, message = ex.Message }); }
+    catch (UnauthorizedAccessException ex) { return BadRequest(new { success = false, message = ex.Message }); }
+    catch (System.Exception)
+    {
+      return StatusCode(500, new { success = false, message = "An error occurred while accepting for contract" });
+    }
+  }
+
+}
---
+++ backend/Api/Services/ContractsService.cs
@@ -0,0 +1,309 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ContractsService
+{
+  private readonly AppDbContext _context;
+
+  public ContractsService(AppDbContext context)
+  {
+    _context = context;
+  }
+
+
+// Future: Create one additional method to get all own contracts
+  public async Task<List<ContractDto>> GetAllContractsAsync(Guid? authorId = null)
+  {
+
+    var contracts = await _context.Contracts
+      .Where(c => authorId == null || c.AuthorId == authorId)
+      .ToListAsync();
+    var contractsIds = contracts.Select(c => c.Id).ToList();
+
+    var applications = await _context.ContractApplications
+      .Where(a => contractsIds.Contains(a.ContractId))
+      .ToListAsync();
+
+    var applicationsByContractId = applications
+      .GroupBy(a => a.ContractId)
+      .ToDictionary(g => g.Key, g => g.ToList());
+
+    var userIds = applications.Select(a => a.UserId).Distinct().ToList();
+
+    var users = await _context.Users
+      .Where(u => userIds.Contains(u.Id))
+      .Select(u => new { u.Id, u.Email })
+      .ToDictionaryAsync(u => u.Id);
+
+    var userData = await _context.UserData
+      .ToDictionaryAsync(u => u.UserId);
+
+    var dto = contracts.Select(c => new ContractDto
+    {
+      Id = c.Id,
+      AuthorId = c.AuthorId,
+      Price = c.Price,
+      Status = c.Status,
+      Description = c.Description,
+      CreatedAt = c.CreatedAt,
+      UpdatedAt = c.UpdatedAt,
+      Deadline = c.Deadline,
+
+      Applications = authorId == c.AuthorId && applicationsByContractId.TryGetValue(c.Id, out var applications)
+        ? applications
+          .Where(a => a.ContractId == c.Id)
+          .Select(a =>
+          {
+            users.TryGetValue(a.UserId, out var user);
+            userData.TryGetValue(a.UserId, out var ud);
+
+            return new ContractApplicationDto
+            {
+              UserId = a.UserId,
+              Email = user?.Email ?? "",
+              UserName = ud != null
+                ? $"{ud.FirstName} {ud.LastName}"
+                : user?.Email ?? "",
+              AppliedAt = a.AppliedAt
+            };
+          }).ToList()
+        : new List<ContractApplicationDto>()
+    }).ToList();
+
+    return dto;
+  }
+
+  public async Task<ContractDto> GetContractByIdAsync(Guid contractId, Guid? authorId = null)
+  {
+    var contract = await _context.Contracts
+      .FirstOrDefaultAsync(c => c.Id == contractId)
+      ?? throw new KeyNotFoundException($"Contract with ID: {contractId} not found");
+
+    var applications = await _context.ContractApplications
+      .Where(a => a.ContractId == contractId)
+      .ToListAsync();
+
+    var userIds = applications.Select(a => a.UserId).Distinct().ToList();
+
+    var users = await _context.Users
+      .Where(u => userIds.Contains(u.Id))
+      .Select(u => new { u.Id, u.Email })
+      .ToDictionaryAsync(u => u.Id);
+
+    var userData = await _context.UserData.ToDictionaryAsync(u => u.UserId);
+
+    var dto = new ContractDto
+    {
+      Id = contract.Id,
+      AuthorId = contract.AuthorId,
+      Price = contract.Price,
+      Status = contract.Status,
+      Description = contract.Description,
+      CreatedAt = contract.CreatedAt,
+      UpdatedAt = contract.UpdatedAt,
+      Deadline = contract.Deadline,
+      Applications = (authorId == contract.AuthorId)
+        ? applications.Select(a =>
+        {
+          users.TryGetValue(a.UserId, out var user);
+          userData.TryGetValue(a.UserId, out var ud);
+
+          return new ContractApplicationDto
+          {
+            UserId = a.UserId,
+            Email = user?.Email ?? "",
+            UserName = ud != null ? $"{ud.FirstName} {ud.LastName}" : user?.Email ?? "",
+            AppliedAt = a.AppliedAt
+          };
+        }).ToList()
+        : new List<ContractApplicationDto>()
+    };
+
+    return dto;
+  }
+
+  public async Task<ContractDto> CreateContractAsync(ContractModel contract, Guid userId)
+  {
+    if (contract == null)
+      throw new ArgumentNullException(nameof(contract), "Contract must have a value.");
+    if (userId == Guid.Empty)
+      throw new ArgumentException("UserId must have a value");
+    if (contract.Price <= 0)
+      throw new ArgumentException("Price must be greater than zero.");
+
+    if (string.IsNullOrWhiteSpace(contract.Description))
+      throw new ArgumentNullException(nameof(contract.Description), "A contract must have a description");
+
+    var ctrc = new ContractModel
+    {
+      AuthorId = userId,
+      Price = contract.Price,
+      Description = contract.Description,
+      Status = StatusOfContractModel.Pending,
+      CreatedAt = DateTime.UtcNow,
+      UpdatedAt = DateTime.UtcNow,
+      Deadline = contract.Deadline ?? DateTime.UtcNow.AddDays(30)
+    };
+      
+      
+    var added = await _context.Contracts.AddAsync(ctrc);
+    await _context.SaveChangesAsync();
+
+
+    var userContract = new ContractDto
+    {
+      Id = ctrc.Id,
+      AuthorId = ctrc.AuthorId,
+      Price = ctrc.Price,
+      Description = ctrc.Description,
+      Status = ctrc.Status,
+      CreatedAt = ctrc.CreatedAt,
+      UpdatedAt = ctrc.UpdatedAt,
+      Deadline = ctrc.Deadline
+    };
+
+    return userContract;
+  }
+
+// Create the accept and close methods later
+  // public async Task<bool> UpdateContractAsync(int id, ContractRequestModel request, int userId)
+  // {
+  //   if (request == null)
+  //     throw new ArgumentNullException(nameof(request), "Request must have a value.");
+
+  //   try
+  //   {
+
+  //     var contract = await _context.Contracts
+  //       .FirstOrDefaultAsync(c => c.Id == id)
+  //       ?? throw new KeyNotFoundException($"Contract with ID {id} not found.");
+
+  //     if (contract.AuthorId != userId)
+  //       throw new UnauthorizedAccessException("You are not authorized to update this contract.");
+
+  //     contract.Price = request.Price > 0 ? request.Price : contract.Price;
+  //     contract.Status = Enum.IsDefined(typeof(StatusOfContractModel), request.Status) ? request.Status : contract.Status;
+  //     contract.Description = !string.IsNullOrWhiteSpace(request.Description) ? request.Description : contract.Description;
+  //     contract.Deadline = request.Deadline ?? contract.Deadline;
+
+  //     await _context.SaveChangesAsync();
+
+  //     return true;
+  //   }
+  //   catch (System.Exception)
+  //   {
+  //     throw;
+  //   }
+  // }
+
+  // public async Task<bool> TakeContractAsync(int id, int userId)
+  // {
+  //   if (id <= 0 || userId <= 0)
+  //     throw new ArgumentException("ID must be greater than 0");
+
+  //   try
+  //   {
+  //     var contract = await _context.Contracts
+  //       .FirstOrDefaultAsync(c => c.Id == id)
+  //       ?? throw new KeyNotFoundException($"Contract with ID {id} not found.");
+
+  //     contract.TargetId = userId;
+  //     await _context.SaveChangesAsync();
+  //     return true;
+  //   }
+  //   catch (System.Exception)
+  //   {
+  //     throw;
+  //   }
+  // }
+  public async Task<bool> DeleteContractAsync(Guid contractId, Guid userId)
+  {
+    if (contractId == Guid.Empty)
+      throw new ArgumentOutOfRangeException("Contract ID is empty");
+    if (userId == Guid.Empty)
+      throw new ArgumentOutOfRangeException("User ID is empty");
+
+    var contract = await _context.Contracts
+      .FirstOrDefaultAsync(c => c.Id == contractId)
+      ?? throw new KeyNotFoundException($"Not found contract with ID: {contractId}");
+
+    if (contract.AuthorId != userId)
+      throw new UnauthorizedAccessException("You are not allowed to delete this contract");
+
+    _context.Contracts.Remove(contract);
+    await _context.SaveChangesAsync();
+
+    return true;
+  }
+
+
+// Future: Divide method for saveing and move dto to other method / controller 
+  public async Task<ContractApplicationDto> ApplyForContractAsync(Guid contractId, Guid userId)
+  {
+    if (contractId == Guid.Empty || userId == Guid.Empty)
+      throw new ArgumentException("Invalid contract ID or user ID");
+
+    var isApplied = await _context.ContractApplications
+      .FirstOrDefaultAsync(ca => ca.UserId == userId && ca.ContractId == contractId);
+
+    if (isApplied != null)
+      throw new ArgumentException($"User with ID: {userId} already applied for contract {contractId}");
+
+    var userData = await _context.UserData
+      .FirstOrDefaultAsync(ud => ud.UserId == userId)
+      ?? throw new KeyNotFoundException($"User data for user ID: {userId} not found.");
+
+    var user = await _context.Users
+        .Where(u => u.Id == userId)
+        .Select(u => new ContractApplicationDto
+        {
+          UserId = u.Id,
+          Email = u.Email,
+          UserName = (userData.FirstName ?? "") + " " + (userData.LastName ?? "")
+        })
+        .FirstOrDefaultAsync();
+
+    if (user == null)
+      throw new KeyNotFoundException($"User with ID: {userId} not found.");
+
+    var contractApplicationData = new ContractApplicationModel
+    {
+      ContractId = contractId,
+      UserId = userId,
+      AppliedAt = DateTime.UtcNow
+    };
+
+    await _context.ContractApplications.AddAsync(contractApplicationData);
+    await _context.SaveChangesAsync();
+
+    return user;
+  }
+
+  public async Task<ContractAcceptUserDto> AcceptContractAsync(Guid contractId, Guid userId, Guid authorId)
+  {
+    using var transaction = await _context.Database.BeginTransactionAsync();
+
+      var contract = await _context.Contracts
+        .Where(c => c.Id == contractId)
+        .FirstOrDefaultAsync()
+        ?? throw new KeyNotFoundException($"Contract with ID {contractId} does not exists");
+
+      Console.WriteLine($"AuthorID: {authorId}, Contract AuthorID: {contract.AuthorId}");
+
+      if (contract.AuthorId != authorId)
+        throw new UnauthorizedAccessException($"You are not allowed to use this method");
+
+      contract.TargetId = userId;
+      contract.Status = StatusOfContractModel.InProgress;
+
+      await _context.SaveChangesAsync();
+
+      var response = new ContractAcceptUserDto
+      {
+        UserId = userId,
+        Accpeted = AcceptEnum.Accepted
+      };
+
+      await transaction.CommitAsync();
+      return response;
+  }
+}
