8eb7364b80cdd60d64819956475b131078f535c5
Marcel <marcelpodsiadlo.business@gmail.com>
2026-01-02 18:27:39 +0100

    Created interface and class for hashing and verifiing text

---
+++ backend/Infrastructure/Security/BCryptPasswordHasher.cs
@@ -0,0 +1,8 @@
+public class BCryptPasswordHasher : IPasswordHasher
+{
+  public string Hash(string password)
+    => BCrypt.Net.BCrypt.HashPassword(password, 12);  
+
+  public bool Verify(string password, string hash)
+    => BCrypt.Net.BCrypt.Verify(password, hash); 
+}
