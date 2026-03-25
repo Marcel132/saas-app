89095a8acba231ad6b46150da063ae4e50220ab0
Marcel <marcelpodsiadlo.business@gmail.com>
2026-03-25 13:12:10 +0100

    Moved files to another folder

--- backend/Domain/Interfaces/Repositories/ISessionRepository.cs
+++ backend/Domain/Interfaces/Repositories/ISessionRepository.cs
@@ -3,4 +3,5 @@ public interface ISessionRepository
   Task<IReadOnlyCollection<Session>> GetActiveByUserIdAsync(Guid userId);
   Task AddAsync(Session sess);
   Task UpdateAsync(Session sess);
+  Task<Session?> GetSessionByRefreshTokenAsync(string refreshToken);
 }
