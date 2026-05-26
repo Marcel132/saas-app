2b3496d50b2380876ce9e0e4a7c5a77d721e72a2
Marcel <marcelpodsiadlo.business@gmail.com>
2026-05-26 16:34:53 +0200

    Add public access modifiers to methods in IContractQueryRepository and IUserQueryRepository interfaces for consistency

--- backend/Domain/Interfaces/Repositories/Contract/IContractQueryRepository.cs
+++ backend/Domain/Interfaces/Repositories/Contract/IContractQueryRepository.cs
@@ -3,4 +3,5 @@ public interface IContractQueryRepository
   public Task<PagedResponse<ContractResponseDto>> GetContractsAsync(int page, int pageSize, string? search);
   public Task<ContractResponseDto?> GetContractsByIdAsync(long contractId);
   public Task<List<ContractApplicationsDto>> GetContractApplicationsAsync(long contractId);
+
 }
--- backend/Domain/Interfaces/Repositories/User/IUserQueryRepository.cs
+++ backend/Domain/Interfaces/Repositories/User/IUserQueryRepository.cs
@@ -1,8 +1,9 @@
 public interface IUserQueryRepository
 {
-  Task<PagedResponse<UserResponsePublicDto>> GetAllAsync(int page, int pageSize, string? search = null);
-  Task<UserResponsePublicDto> GetUserByIdAsync(Guid userId);
-  Task<UserResponsePrivateDto> GetCurrentUserByIdAsync(Guid userId);
-  Task<List<UserContractsDto>> GetCurrentUserContractsAsync(Guid userId, ContractStatus? status = null);
+  public Task<PagedResponse<UserResponsePublicDto>> GetAllAsync(int page, int pageSize, string? search = null);
+  public Task<UserResponsePublicDto> GetUserByIdAsync(Guid userId);
+  public Task<UserResponsePrivateDto> GetCurrentUserByIdAsync(Guid userId);
+  public Task<List<UserContractsDto>> GetCurrentUserContractsAsync(Guid userId, ContractStatus? status = null);
+  public Task<List<UserApplicationsDto>> GetApplicationsAsync(Guid userId, ContractApplicationStatus? status);
 
 }
