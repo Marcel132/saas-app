8318e221480494dd55d30a7ffa6e47ac147e2c40
Marcel <marcelpodsiadlo.business@gmail.com>
2026-06-01 21:45:59 +0200

    feat: Add new layout

---
+++ saas-platform/src/app/features/dashboard/components/company-dashboard/company-dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-dashboard',
+  imports: [],
+  templateUrl: './company-dashboard.html',
+  styleUrl: './company-dashboard.scss',
+})
+export class CompanyDashboard {}
--- saas-platform/src/app/features/dashboard/pages/dashboard-page/dashboard-page.ts
+++ saas-platform/src/app/features/dashboard/pages/dashboard-page/dashboard-page.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MainStore } from '../store/main.store';
+import { RoleType } from '../../../auth/models/role-type.enum';
+import { PentesterDashboard } from '../../components/pentester-dashboard/pentester-dashboard';
+import { CompanyDashboard } from '../../components/company-dashboard/company-dashboard';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [
+    PentesterDashboard,
+    CompanyDashboard
+  ],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
 })
-export class DashboardPage {}
+export class DashboardPage {
+
+  private readonly mainStore = inject(MainStore);
+
+  readonly currentUser = this.mainStore.currentUser;
+  readonly roleType = RoleType;
+
+  ngOnInit() {
+    this.mainStore.loadCurrentUser()
+  }
+}
