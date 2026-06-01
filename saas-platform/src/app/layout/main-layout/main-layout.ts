949041a5a6d2c446dfd854f4e32a98044ad9c6ad
Marcel <marcelpodsiadlo.business@gmail.com>
2026-06-01 22:28:52 +0200

    feat: create default styles for layout

--- saas-platform/src/app/layout/main-layout/main-layout.ts
+++ saas-platform/src/app/layout/main-layout/main-layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 
 @Component({
@@ -7,4 +7,11 @@ import { RouterOutlet } from "@angular/router";
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
-export class MainLayout {}
+export class MainLayout {
+
+  readonly isSidebarCollapsed = signal(false);
+
+  toogleSidebar() {
+    this.isSidebarCollapsed.update(value => !value);
+  }
+}
