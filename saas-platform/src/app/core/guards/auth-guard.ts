53a5ae4d9254aa5559d6312ea2c9da7971fcc6eb
Marcel <marcelpodsiadlo.business@gmail.com>
2026-06-02 12:12:10 +0200

    feat: created guards

---
+++ saas-platform/src/app/core/guards/auth-guard.ts
@@ -0,0 +1,11 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthStore } from '../../features/auth/store/auth.store';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authStore = inject(AuthStore);
+  const router = inject(Router);
+
+  return authStore.currentUser() ? true : router.createUrlTree(['/auth/login']);
+
+};
---
+++ saas-platform/src/app/core/guards/guest-guard.ts
@@ -0,0 +1,10 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthStore } from '../../features/auth/store/auth.store';
+
+export const guestGuard: CanActivateFn = (route, state) => {
+  const authStore = inject(AuthStore);
+  const router = inject(Router);
+
+  return !authStore.currentUser() ? true : router.createUrlTree(['/app']);
+};
