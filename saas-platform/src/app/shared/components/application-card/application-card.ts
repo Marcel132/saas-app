bee1f0171e5383056b028116829f124964c5dfff
Marcel <marcelpodsiadlo.business@gmail.com>
2026-06-10 12:34:53 +0200

    feat: refactor assignments and offers pages to use PentesterStore; update application card routing and styles

--- saas-platform/src/app/shared/components/application-card/application-card.ts
+++ saas-platform/src/app/shared/components/application-card/application-card.ts
@@ -1,13 +1,15 @@
 import { Component, input } from '@angular/core';
 import { CurrencyPipe } from '../../pipes/currency-pipe';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-application-card',
   imports: [
     CurrencyPipe,
-    DatePipe
-  ],
+    DatePipe,
+    RouterLink
+],
   templateUrl: './application-card.html',
   styleUrl: './application-card.scss',
 })
@@ -16,4 +18,5 @@ export class ApplicationCard {
   price = input.required<number>()
   status = input.required<string>()
   appliedAt = input.required<Date>()
+  route = input.required<string>()
 }
