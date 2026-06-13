d71131e8f96e6fbd449c6ce404e69ac41bb178e8
Marcel <marcelpodsiadlo.business@gmail.com>
2026-06-13 15:22:00 +0200

    feat: refactor CompanyStore to improve request handling and error management in contract operations

--- saas-platform/src/app/features/main/store/company.store.ts
+++ saas-platform/src/app/features/main/store/company.store.ts
@@ -1,142 +1,209 @@
 import { inject, Injectable, signal } from "@angular/core";
+import { catchError, switchMap, tap, throwError } from "rxjs";
+
 import { ContractDto } from "../models/contract-dto";
 import { MeApi } from "../../../core/services/me-api";
 import { ContractApi } from "../../../core/services/contract-api";
 import { EditContractDto } from "../pages/company-layout/contracts/models/edit-contract-dto";
-import { tap } from "rxjs";
 import { AddContractDto } from "../pages/company-layout/contracts/models/add-contract-dto";
 import { CompanyApplicationsDto } from "../models/company-applications-dto";
 import { ApplicationApi } from "../../../core/services/application-api";
+import { RequestState } from "../../../core/models/request-state";
+import { CONTRACT_STATUS_ORDER } from "../../auth/constants/contract-status-order";
+import { APPLICATIONS_STATUS_ORDER } from "../../auth/constants/application-status-order";
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class CompanyStore{
+export class CompanyStore {
   // DI
   private readonly meApi = inject(MeApi);
   private readonly contractApi = inject(ContractApi);
   private readonly applicationApi = inject(ApplicationApi);
 
   // SIGNALS
-  readonly isLoading = signal<boolean>(false)
+  request = signal<RequestState>({
+    state: 'idle',
+    message: ''
+  })
+
   readonly contracts = signal<ContractDto[]>([])
   readonly selectedContract = signal<ContractDto | null>(null)
 
   readonly applications = signal<CompanyApplicationsDto[]>([])
 
-  getContracts(){
-
-    const statusOrder = {
-      InProgress: 0,
-      Open: 1,
-      Completed: 2,
-      Cancelled: 3,
-    }
-    this.isLoading.set(true)
-    this.meApi.getContracts()
-    .pipe()
-    .subscribe({
-      next: res =>  {
-        if(res.data){
-          res.data.items.sort(
-            (a,b) => statusOrder[a.contractStatus] - statusOrder[b.contractStatus]
-          )
-          this.contracts.set(res.data.items);
-        }
-
-        this.isLoading.set(false)
-        console.log(res)
-      },
-      error: err => {
-        this.isLoading.set(false)
-        console.log(err)
-      }
+  getContracts() {
+    this.request.set({
+      state: 'loading',
+      message: "Ładowanie kontraktów..."
     })
+
+    return this.meApi.getContracts()
+      .pipe(
+        tap(res => {
+          if (res.data) {
+            res.data.items.sort(
+              (a, b) => CONTRACT_STATUS_ORDER[a.contractStatus] - CONTRACT_STATUS_ORDER[b.contractStatus]
+            )
+
+            this.contracts.set(res.data.items)
+            this.request.set({
+              state: 'success',
+              message: 'Pobrano Kontrakty'
+            })
+          }
+        }),
+        catchError(error => {
+          this.request.set({
+            state: 'error',
+            message: error.error.message
+          })
+
+          return throwError(() => error)
+        })
+      )
   }
-  getContractById(id: number){
-    this.isLoading.set(true)
-    this.contractApi.getContractById(id).subscribe({
-      next: res => {
-        this.selectedContract.set(res.data)
-        this.isLoading.set(false)
-      },
-      error: err => {
-        this.isLoading.set(false)
-        console.log(err)
-      }
+  getContractById(id: number) {
+    this.request.set({
+      state: 'loading',
+      message: "Ładowanie kontraktów..."
     })
+    return this.contractApi.getContractById(id)
+      .pipe(
+        tap(res => {
+          if (res.data)
+            this.selectedContract.set(res.data)
+
+          this.request.set({
+            state: 'success',
+            message: `Pobrano Kontrakt ${id}`
+          })
+        }),
+        catchError(error => {
+          this.request.set({
+            state: 'error',
+            message: error.error.message
+          })
+
+          return throwError(() => error)
+        })
+      )
   }
-  updateContract(id: number, form: EditContractDto){
-    return this.contractApi.updateContract(id, form).pipe(
-      tap( res => console.log(res.message))
-    )
+  updateContract(id: number, form: EditContractDto) {
+    return this.contractApi.updateContract(id, form)
+      .pipe(
+        tap(res =>{
+          this.request.set({
+            state: 'success',
+            message: res.message ?? "Zaktualizowano kontrakt"
+          })
+        })
+      )
   }
 
-  addContract(form: AddContractDto){
-    return this.contractApi.craeteContract(form).pipe()
+  addContract(form: AddContractDto) {
+    return this.contractApi.createContract(form)
+      .pipe(
+        tap(res =>{
+          this.request.set({
+            state: 'success',
+            message: res.message ?? "Dodano Kontrakt"
+          })
+        }),
+        catchError(error => {
+          this.request.set({
+            state: 'error',
+            message: error.error.message ?? "Błąd w danych formularza"
+          })
+
+          return throwError(() => error)
+        })
+      )
   }
 
-  deleteContract(id: number){
-    this.contractApi.deleteContract(id).pipe(
-      tap(res => {
-        console.log(res)
+  deleteContract(id: number) {
+    return this.contractApi.deleteContractSoft(id)
+      .pipe(
+        tap(res => {
+          console.log(res)
 
-        this.contracts.update(contracts =>
-          contracts.map(contract =>
-            contract.contractId === id
-            ? {
-              ...contract,
-              contractStatus: 'Cancelled'
-              }
-            : contract
+          this.contracts.update(contracts =>
+            contracts.map(contract =>
+              contract.contractId === id
+                ? {
+                  ...contract,
+                  contractStatus: 'Cancelled'
+                }
+                : contract
+            )
           )
-        )
-      })
-    ).subscribe()
+        })
+      )
   }
 
   getContractApplications(id: number) {
-    const statusOrder = {
-      Accepted: 0,
-      Pending: 1,
-      Rejected: 2
-    }
-    this.contractApi.getContractApplications(id)
-    .pipe()
-    .subscribe({
-      next: res => {
-        if(res.data){
-          res.data.sort(
-            (a,b) => statusOrder[a.status] - statusOrder[b.status]
-          )
-          this.applications.set(res.data)
-        }
-      }
-    })
+    return this.contractApi.getContractApplications(id)
+      .pipe(
+        tap(res => {
+          if (res.data) {
+            res.data.sort(
+              (a, b) => APPLICATIONS_STATUS_ORDER[a.status] - APPLICATIONS_STATUS_ORDER[b.status]
+            )
+            this.applications.set(res.data)
+          }
+        })
+      )
   }
 
-  acceptApplication(applicationId: number, contractId: number){
-    this.applicationApi.acceptApplication(applicationId)
-    .pipe()
-    .subscribe({
-      next: res => {
-        if(res.success){
-          this.getContractApplications(contractId)
-        }
-      }
-    })
+  acceptApplication(applicationId: number, contractId: number) {
+    return this.applicationApi.acceptApplication(applicationId)
+      .pipe(
+        tap(res =>{
+          this.request.set({
+            state: 'success',
+            message: res.message ?? "Dodano Kontrakt"
+          })
+        }),
+        switchMap(() => this.getContractApplications(contractId)),
+        catchError(error => {
+          this.request.set({
+            state: 'error',
+            message: error.error.message ?? "Nie można zaakceptować tej samej aplikacji"
+          })
+
+          return throwError(() => error)
+        })
+      )
   }
 
-  rejectApplication(applicationId: number, contractId: number){
-    this.applicationApi.rejectApplication(applicationId)
-    .pipe()
-    .subscribe({
-      next: res => {
-        if(res.success){
-          this.getContractApplications(contractId)
-        }
-      }
-    })
+  rejectApplication(applicationId: number) {
+    return this.applicationApi.rejectApplication(applicationId)
+      .pipe(
+        tap(res => {
+          this.applications.update(applications =>
+            applications.map(app =>
+              app.applicationId === applicationId
+                ? {
+                  ...app,
+                  status: 'Rejected'
+                }
+                : app
+            )
+          );
+          this.request.set({
+            state: 'success',
+            message: res.message ?? "Dodano Kontrakt"
+          })
+        }),
+        catchError(error => {
+          this.request.set({
+            state: 'error',
+            message: error.error.message ?? "Błąd odrzucenia"
+          })
+
+          return throwError(() => error)
+        })
+      )
   }
 }
