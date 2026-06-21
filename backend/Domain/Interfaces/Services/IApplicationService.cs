af06ca6167709067d9a1bfd3383bb6974f9cb081
Marcel <marcelpodsiadlo.business@gmail.com>
2026-06-21 16:59:22 +0200

    feat: create IApplicationService interface for application management

---
+++ backend/Domain/Interfaces/Services/IApplicationService.cs
@@ -0,0 +1,10 @@
+using backend.Api.Controllers.Applications.DTOs;
+
+namespace backend.Domain.Interfaces.Services;
+
+public interface IApplicationService
+{
+  public Task AcceptApplicationAsync(Guid userId, long applicationId);
+  public Task RejectApplicationAsync(Guid userId, long applicationId);
+  public Task<List<ApplicationDto>> GetCurrentUserApplications(Guid userId);
+}
