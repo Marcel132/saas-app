3ed13b5b52058ebab434318e8b1b007cca5cec7f
Marcel <marcelpodsiadlo.business@gmail.com>
2026-07-02 18:49:01 +0200

    refactor: Add Angular Material components and styles for info tooltip feature

---
+++ saas-platform/src/app/shared/ui/info-tooltip/info-tooltip.ts
@@ -0,0 +1,18 @@
+import { Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@Component({
+  selector: 'app-info-tooltip',
+  imports: [
+    MatIconModule,
+    MatTooltipModule
+  ],
+  templateUrl: './info-tooltip.html',
+  styleUrl: './info-tooltip.scss',
+})
+export class InfoTooltip {
+
+  text = input.required<string>()
+
+}
