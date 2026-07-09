4764476be97f64d9033d509f646a2fa4901cd986
Marcel <marcelpodsiadlo.business@gmail.com>
2026-07-09 17:45:49 +0200

    feat: implement request form with validation and toggle functionality in RequestCard component

--- saas-platform/src/app/features/main/pages/company-layout/contracts/components/request-card/request-card.ts
+++ saas-platform/src/app/features/main/pages/company-layout/contracts/components/request-card/request-card.ts
@@ -1,11 +1,64 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-request-card',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './request-card.html',
   styleUrl: './request-card.scss',
 })
 export class RequestCard {
   number = input.required<number>();
+  showRequest = signal(false);
+
+  form = new FormGroup({
+    title: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.maxLength(256)
+      ],
+      nonNullable: true,
+    }),
+    url: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.maxLength(256),
+        Validators.pattern(/^https?:\/\/.+/)
+      ],
+      nonNullable: true,
+    }),
+    scope: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.maxLength(1000)
+      ],
+      nonNullable: true
+    }),
+    credentials: new FormControl('Email: \nPassword:  ', {
+      validators: [
+        Validators.required,
+        Validators.pattern(/^Email:\s*.+\nPassword:\s*.+$/)
+      ],
+      nonNullable: true
+    }),
+    deadline: new FormControl('', {
+      validators: [
+        Validators.required,
+      ],
+      nonNullable: true
+    })
+  })
+
+  toggleRequest(){
+    this.showRequest.update(x => !x)
+  }
+
+  createRequest(){
+    if(this.form.invalid)
+      return;
+
+    console.log(this.form.value)
+  }
 }
