16bbf9eed91b83dfbed9078ac843e6981f2eff40
Marcel <marcelpodsiadlo.business@gmail.com>
2026-07-12 16:09:49 +0200

    feat: remove AuthService, UserRegisterService, and IAuthService interface to streamline authentication process

--- backend/Domain/Interfaces/Features/IAuthService.cs
+++
@@ -1,8 +0,0 @@
-namespace backend.Domain.Interfaces.Features;
-
-public interface IAuthService
-{
-  public Task LogoutAsync(Guid userId);
-
-  public Task<CredentialsDto> RefreshTokenAsync(string ipAddress, string userAgent, string? refreshToken);
-}
